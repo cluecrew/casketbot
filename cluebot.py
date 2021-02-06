45e0fe873ff260c0448823508b42357433b6fdef
Swanson <8145783-Swans0n@users.noreply.gitlab.com>
2021-02-06 01:54:31 +0000

    Update cluebot.py

--- cluebot.py
+++ cluebot.py
@@ -1,6 +1,7 @@
 # cluebot.py
 import os
 import discord
+from rs3_api import Hiscores
 
 from dotenv import load_dotenv
 
@@ -14,4 +15,12 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 client.run(Token)
+
+def test_get_index_lite(self, hiscore):
+        response = hiscore.get_index_lite("normal", "Clue Crew")
+        assert isinstance(response, dict)
+
+print(response)
+
+
 
