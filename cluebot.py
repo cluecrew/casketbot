475f70ae9aad7bad3e769087e1454d8e0316ca81
Swanson <8145783-Swans0n@users.noreply.gitlab.com>
2021-02-07 21:56:52 +0000

    Update cluebot.py

--- cluebot.py
+++ cluebot.py
@@ -17,10 +17,9 @@ def clueStatsLookup(name):
     username = str(name)
     clueStatsLookup.urlwithusername = beginnerUrl + username
     column_names = ["Rank", "Clues", "XP"]
-    print(clueStatsLookup.urlwithusername+ " will be the URL for this request!")
+    #print(clueStatsLookup.urlwithusername+ " will be the URL for this request!")
     url = clueStatsLookup.urlwithusername
     stats = panda.read_csv(url, names=column_names)
-    print(stats)
     print("Stats found for "+username)
     Ranks = stats.Rank.to_list() 
     Clues = stats.Clues.to_list()
@@ -37,7 +36,6 @@ def clueStatsLookup(name):
     clueStatsLookup.eliteRank = Ranks[57]
     clueStatsLookup.masterRank = Ranks[58]
     clueStatsLookup.totalClues = clueStatsLookup.easyClueCount + clueStatsLookup.mediumClueCount + clueStatsLookup.hardClueCount + clueStatsLookup.eliteClueCount + clueStatsLookup.masterClueCount
-    print("Successful function for "+ username)
 
 #client = discord.Client()
 bot = commands.Bot(command_prefix='=')
@@ -62,8 +60,8 @@ async def setRsn(ctx, *args):
     usernameDesired = ""
     for arg in args:
         usernameDesired = usernameDesired + "_" + arg
-    print(usernameDesired+" is the desired username!")
-    print(ctx.author.display_name)
+    #print(usernameDesired+" is the desired username!")
+    #print(ctx.author.display_name)
     embed = discord.Embed(title="Set Username for " + ctx.author.display_name, url=clueStatsLookup.urlwithusername, description="You have assigned" + usernameDesired + " to your profile!" , color=discord.Color.blue())
     embed.set_author(name=ctx.author.display_name,icon_url=ctx.author.avatar_url)
     embed.set_thumbnail(url="https://i.ibb.co/jy4nvMV/thumbnail10.png")
@@ -78,9 +76,9 @@ async def clueCheck(ctx, *args):
     usernameForLookup = ""
     for arg in args:
         usernameForLookup = usernameForLookup + "_" + arg
-    print(usernameForLookup)
+    #print(usernameForLookup)
     clueStatsLookup(usernameForLookup)
-    print("Stats looked up!")
+    #print("Stats looked up!")
     #embed response into a message block that looks professional
     embed = discord.Embed(title="Clue stats for " + ctx.author.display_name, url=clueStatsLookup.urlwithusername, description="Successful Search!", color=discord.Color.blue())
     embed.set_author(name=ctx.author.display_name,icon_url=ctx.author.avatar_url)
@@ -90,25 +88,20 @@ async def clueCheck(ctx, *args):
     embed.set_footer(text = time_string)
     clueCounter = [clueStatsLookup.easyClueCount,clueStatsLookup.mediumClueCount,clueStatsLookup.hardClueCount,clueStatsLookup.eliteClueCount,clueStatsLookup.masterClueCount]
     pointTotal = 0
-    print(clueCounter)
+    #print(clueCounter)
     for x in range(5):
         pointTotal = pointTotal + math.pow(2, x) * ((math.floor(clueCounter[x]/500) * math.pow(2,5)) + (math.floor(clueCounter[x]/250) - math.floor(clueCounter[x]/500)) * math.pow(2,4) + (math.floor(clueCounter[x]/100) - math.floor(clueCounter[x]/500)) * math.pow(2,3) + (math.floor(clueCounter[x]/50) - math.floor(clueCounter[x]/100) - math.floor(clueCounter[x]/250) + math.floor(clueCounter[x]/500)) * math.pow(2,2) + (math.floor(clueCounter[x]/10) - math.floor(clueCounter[x]/50)) * math.pow(2,1) + (clueCounter[x]-math.floor(clueCounter[x]/10)) * math.pow(2,0))
-        print(pointTotal)
-    print(pointTotal)
+        #print(pointTotal)
+    #print(pointTotal)
     pointTotal = int(pointTotal)
     embed.add_field(name="Total Clue Points", value="Total Clue Points: "+ str(pointTotal))
     embed.add_field(name="Total Clues Completed",value="Total Clues: " + str(clueStatsLookup.totalClues))
-       
-    
-    
+    embed.add_field(name="Master Clues",value="Count: "+str(clueStatsLookup.masterClueCount)+","+" Rank: "+str(clueStatsLookup.masterRank),inline=False)
+    embed.add_field(name="Elite Clues",value="Count: "+str(clueStatsLookup.eliteClueCount)+","+" Rank: "+str(clueStatsLookup.eliteRank),inline=False)
+    embed.add_field(name="Hard Clues",value="Count: "+str(clueStatsLookup.hardClueCount)+","+" Rank: "+str(clueStatsLookup.hardRank),inline=False)
+    embed.add_field(name="Medium Clues",value="Count: "+str(clueStatsLookup.mediumClueCount)+","+" Rank: "+str(clueStatsLookup.mediumRank),inline=False)
+    embed.add_field(name="Easy Clues",value="Count: "+str(clueStatsLookup.easyClueCount)+","+" Rank: "+str(clueStatsLookup.easyRank),inline=False)
     
     await ctx.send(embed=embed)    
 
 bot.run(Token)
-
-
-
-
-
-
-
