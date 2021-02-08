e1b15d95587eeeae4b31e4c28b7e3a68dc8f84f0
Swans0n <spencerpolsgrove@gmail.com>
2021-02-08 11:59:12 -0500

    Fixed the Drop Table Logic. All commands should work proporly now.

--- hardDropTable.py
+++ hardDropTable.py
@@ -1,47 +1,49 @@
 # hardDropTable.py
 import os
+import random
 def hardDrop():
-    rollHard = randint(1,96)
+    rollHard = random.randint(1,96)
     print("Rolled a "+str(rollHard)+"!")
-    If rollHard == 1 if rollHard <= 15
+    if  1 <= rollHard <= 15:
         print("Any God Trimmed Armor")
-    If rollHard == 16 if rollHard <= 25
+    elif  16 <= rollHard <= 25:
         print("Any Trimmed Rune Armor")
-    If rollHard == 26 if rollHard <= 50
+    elif  26 <= rollHard <= 50:
         print("Any Heraldic Rune Armor")
-    If rollHard == 51 if rollHard <= 54
+    elif  51 <= rollHard <= 54:
         print("Any Trimmed Dragonhide")
-    If rollHard == 55 if rollHard <= 57
+    elif  55 <= rollHard <= 57:
         print("Any Enchanted Outfit Piece")
-    If rollHard == 58 if rollHard <= 69
+    elif  58 <= rollHard <= 69:
         print("Any Vestment Piece")
-    If rollHard == 70 if rollHard <= 81
+    elif  70 <= rollHard <= 81:
         print("Any Blessed Dragonhide Piece")
-    If rollHard == 82 if rollHard <= 84
+    elif  82 <= rollHard <= 84:
         print("Any Cavalier")
-    If rollHard == 85
+    elif rollHard == 85:
         print("Pirate Hat")
-    If rollHard == 86
+    elif rollHard == 86:
         print("Robinhood Hat")
-    If rollHard == 87
+    elif rollHard == 87:
         print("Amulet of Fury (t)")
-    If rollHard == 88
+    elif rollHard == 88:
         print("Amulet of Glory (t)")
-    If rollHard == 89
+    elif rollHard == 89:
         print("Top Hat")
-    If rollHard == 90
+    elif rollHard == 90:
         print("Rune Cane")
-    If rollHard == 91
+    elif rollHard == 91:
         print("Animal Mask Table")
-    If rollHard == 92
+    elif rollHard == 92:
         print("Dragon Mask Table")
-    If rollHard == 93
+    elif rollHard == 93:
         print("Sack of Effigies Table")
-    If rollHard == 94
+    elif rollHard == 94:
         print("Backstab Table")
-    If rollHard == 95
+    elif rollHard == 95:
         print("Dye Table")
-    If rollHard == 96
+    elif rollHard == 96:
         print("Mega-Rare Table")
     print("Drop Complete! - Hard Drop Table")
     
+hardDrop()
